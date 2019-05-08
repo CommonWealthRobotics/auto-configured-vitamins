775e97a070245a7aca8e4cc1298356e32b397b64
Kevin Harrington <mad.hephaestus@gmail.com>
2019-05-07 23:54:42 -0400

    Adding a launcher of the arm

--- launch.groovy
+++ launch.groovy
@@ -1,1 +1,8 @@
 // load the robot
+def base =DeviceManager.getSpecificDevice( "HephaestusArm",{
+	return ScriptingEngine.gitScriptRun(
+            "https://github.com/madhephaestus/SeriesElasticActuator.git", // git location of the library
+            "FullHardwareLaunch.groovy" , // file to load
+            null
+            )
+})
