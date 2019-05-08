fd771990fa66f5ecd39aba8da24b82ed37b88b0e
Kevin Harrington <mad.hephaestus@gmail.com>
2019-05-08 08:17:07 -0400

    local load

--- launch.groovy
+++ launch.groovy
@@ -1,8 +1,8 @@
 // load the robot
 def base =DeviceManager.getSpecificDevice( "HephaestusArm",{
 	return ScriptingEngine.gitScriptRun(
-            "https://github.com/madhephaestus/SeriesElasticActuator.git", // git location of the library
-            "FullHardwareLaunch.groovy" , // file to load
+            "https://github.com/CommonWealthRobotics/auto-configured-vitamins.git", // git location of the library
+            "loadRobot.groovy" , // file to load
             null
             )
 })
@@ -11,3 +11,4 @@ while((indicator =MobileBaseCadManager.get( base).getProcesIndictor().get())<1){
 	println "Waiting for cad to get to 1:"+indicator
 	ThreadUtil.wait(1000)
 }
+
